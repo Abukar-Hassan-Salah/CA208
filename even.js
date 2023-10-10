cc890f95910c1c5d954120732be4b125b092cc9a
Abukar-Hassan-Salah <abukarhassansalah@gmail.com>
2023-10-10 16:05:22 +0300

    new

--- even.js
+++ even.js
@@ -1,8 +1,22 @@
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
 var listener1 = function (){
-    console.log("listener 1 excuted")
+    console.log("listener 1 excuted");
 }
 
-eventEmitter.addListener("connection",listener1)
-eventEmitter.emit("listener1");
+eventEmitter.on("connection",listener1);
+eventEmitter.emit("connection");
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
