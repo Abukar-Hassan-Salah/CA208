760b193d5d6d273f2227de5270b5732122f45308
Abukar-Hassan-Salah <abukarhassansalah@gmail.com>
2023-10-10 15:59:43 +0300

    test

--- even.js
+++ even.js
@@ -3,5 +3,6 @@ var eventEmitter = new events.EventEmitter();
 var listener1 = function (){
     console.log("listener 1 excuted")
 }
+
 eventEmitter.addListener("connection",listener1)
 eventEmitter.emit("listener1");
